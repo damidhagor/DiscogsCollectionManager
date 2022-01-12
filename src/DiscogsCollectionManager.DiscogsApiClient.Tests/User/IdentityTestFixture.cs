386d1459e0711d64fffe4abdae23713248c28db0
Alexander Jurk <alexander.jurk@outlook.com>
2022-01-13 00:05:20 +0100

    Added first DiscogsApiClient unit test

---
+++ src/DiscogsCollectionManager.DiscogsApiClient.Tests/User/IdentityTestFixture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DiscogsCollectionManager.DiscogsApiClient.Tests.User;
+
+[TestFixture]
+public class IdentityTestFixture : ApiBaseTestFixture
+{
+    [Test]
+    public async Task GetIdentity_Success()
+    {
+        var identity = await ApiClient.GetIdentityAsync(default);
+
+        Assert.IsNotNull(identity);
+        Assert.AreEqual("DamIDhagor", identity.Username);
+        Assert.AreEqual("DamIDhagor", identity.ConsumerName);
+        Assert.AreEqual(12579295, identity.Id);
+        Assert.AreEqual("https://api.discogs.com/users/DamIDhagor", identity.ResourceUrl);
+    }
+}
